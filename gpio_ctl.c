75b76f04f645db2428fd699a00b59ed8d3cbf985
Joel Pinto <joelpostman97@gmail.com>
2022-03-28 22:58:16 +0100

    global: Add gpio descriptors initialization and release
    This commit implements the gpio descriptors management (initialization and release).
    The driver uses the legacy API to fetch the pins and then parses it to a descriptor for control.
    The use of the legacy API just the gpio request and release is due to failure on requesting the
    gpios descriptors with the new API.
    
    Signed-off-by: Joel Pinto <joelpostman97@gmail.com>

--- gpio_ctl.c
+++ gpio_ctl.c
@@ -15,6 +15,8 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/slab.h>
+#include <linux/gpio/consumer.h>
+#include <linux/gpio.h>
 /**
  * Kernel log level
  * #include <linux/kern_levels.h>
@@ -38,6 +40,8 @@ static int gpio_ctl_release(struct inode *, struct file *);
 static ssize_t gpio_ctl_read(struct file *file, char __user *buffer, size_t len, loff_t *offset);
 static ssize_t gpio_ctl_write(struct file *file, const char __user *buffer, size_t len, loff_t *offset);
 static long gpio_ctl_ioctl(struct file *file, unsigned int cmd, unsigned long param);
+static int create_gpios_desc(void);
+static void drop_gpios(void);
 
 // C tag struct initialization
 struct file_operations my_fops = {
@@ -49,7 +53,9 @@ struct file_operations my_fops = {
     .release = gpio_ctl_release
 };
 
-static struct gpio_ctl_priv gpio_priv;
+static struct gpio_ctl_priv gpio_priv = {
+    .pins_legacy_id = {23, 24}
+};
 
 /**
  * @brief
@@ -168,10 +174,17 @@ static int __init gpio_ctl_init(void)
         goto failed_device_create;
     }
 
+    ret = create_gpios_desc();
+    if (ret < 0)
+        goto failed_gpios_setup;
+
     return 0;
 
-failed_device_create:
+failed_gpios_setup:
+    drop_gpios();
     device_destroy(gpio_priv.my_class, gpio_priv.my_dev_num);
+failed_device_create:
+    class_destroy(gpio_priv.my_class);
 failed_cdev_add:
     cdev_del(gpio_priv.my_cdev);
 failed_cdev_alloc:
@@ -187,10 +200,74 @@ failed_register_chrdev:
 static void __exit gpio_init_exit(void)
 {
     CDRV_LOG(INFO, "Unloading gpio_ctl module\n");
+    drop_gpios();
     device_destroy(gpio_priv.my_class, gpio_priv.my_dev_num);
     class_destroy(gpio_priv.my_class);
     cdev_del(gpio_priv.my_cdev);
     unregister_chrdev_region(gpio_priv.my_dev_num, GPIO_CTL_DEV_COUNT);
+}
+
+/**
+ * @brief Create a gpios desc object
+ *
+ * @return int
+ */
+static int create_gpios_desc(void)
+{
+    int i;
+    int success = 0;
+
+    for (i = 0; i < GPIO_CTL_GPIOS_LEN; ++i) {
+        int ret;
+
+        ret = gpio_request(gpio_priv.pins_legacy_id[i], NULL);
+        if (ret < 0) {
+            CDRV_LOG(WARNING, "Failed to obtain gpio %u through legacy API, error code %d\n", gpio_priv.pins_legacy_id[i], ret);
+            success = -ENOENT;
+            break;
+        }
+
+        gpio_priv.pins_desc[i] = gpio_to_desc(gpio_priv.pins_legacy_id[i]);
+        if (gpio_priv.pins_desc[i] == NULL) {
+            CDRV_LOG(WARNING, "Failed to cast pin %u to gpio descriptor\n", gpio_priv.pins_legacy_id[i]);
+            success = ENOENT;
+            break;
+        }
+
+        ret = gpiod_direction_output(gpio_priv.pins_desc[i], 0);
+        if (ret < 0) {
+            success = ENOENT;
+            CDRV_LOG(WARNING, "Failed to set direction \n");
+            break;
+        }
+    }
+
+    /**
+    * Unexpected failure.
+    gpio_test = gpiod_get(gpio_priv.my_device, "fod", GPIOD_OUT_LOW);
+    if (IS_ERR(gpio_test)) {
+        ret = (int) PTR_ERR(gpio_test);
+        CDRV_LOG(WARNING, "Failed to obtain specific gpio, error code %d", ret);
+    }
+    */
+
+    return success;
+}
+ 
+/**
+ * @brief
+ *
+ */
+static void drop_gpios(void)
+{
+    int i;
+
+    for (i = 0; i < GPIO_CTL_GPIOS_LEN; ++i) {
+        int pin;
+        pin = desc_to_gpio(gpio_priv.pins_desc[i]);
+        gpio_priv.pins_desc[i] = NULL;
+        gpio_free(pin);
+    }
 }
 
 module_init(gpio_ctl_init);
--- gpio_ctl.h
+++ gpio_ctl.h
@@ -2,10 +2,13 @@
 
 #define GPIO_CTL_MINOR     0
 #define GPIO_CTL_DEV_COUNT 1
+#define GPIO_CTL_GPIOS_LEN 2
 
 struct gpio_ctl_priv {
     dev_t my_dev_num;
+    const uint32_t pins_legacy_id[GPIO_CTL_GPIOS_LEN];
     struct cdev *my_cdev;
     struct class *my_class;
     struct device *my_device;
+    struct gpio_desc *pins_desc[2];
 };
